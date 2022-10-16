2dfc0f5fb16ed025286ba0aa35de90c78ed3643a
tderuvo <tonyderuvo@mac.com>
2022-10-15 22:02:48 -0700

    added components folder and header.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
       <p>above</p>
       {/* Header */}
+      <Header />
       {/* Hero */}
       {/* About */}
       {/* Experience */}
