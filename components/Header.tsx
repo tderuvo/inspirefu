7fb6a974656913566e49ea4d74a0de0a4d56dc21
tderuvo <tonyderuvo@mac.com>
2022-10-19 11:26:31 -0700

    cleaned up formatting

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,27 +5,12 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <motion.div
-      initial={{
-        x: -100,
-        opacity: 0,
-        scale: 0.5,
-      }}
-      animate={{
-        x: 0,
-        opacity: 1,
-        scale: 1,
-      }}
-      transition={{
-        duration: 1.5,
-      }}
-      className="flex flex-row text-center bg-gray-500"
-    >
-      <div className="justify-center items-center">
-        <h1 className="uppercase px-5 py-5 flex-center text-center justify-center text-white max-w-7xl items-center text-2xl tracking-widest">
+    <div>
+      <div className="bg-[rgb(39,39,39)]">
+        <h1 className="uppercase  py-5 text-center text-white max-w-7xl text-2xl tracking-widest">
           InspireFu
         </h1>
       </div>
-    </motion.div>
+    </div>
   );
 }
